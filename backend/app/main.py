18fe0edf021b6500bdb72b740e3c281d0f7d4ec9
abedassi2001 <abedassi134@gmail.com>
2025-12-09 07:12:50 +0200

    prepared the files
    added the backend and the frontend files

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+'''
+@Abed
+ to download the depedencies  read the requirements.txt file 
+'''
+
+
+from fastapi import FastAPI
+from app.api.routes import chat, users, health
+
+app = FastAPI(title="Flutter + FastAPI + OpenAI")
+
+app.include_router(health.router)
+app.include_router(users.router)
+app.include_router(chat.router)
